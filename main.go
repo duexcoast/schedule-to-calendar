786344098c20f0cd21891eb8e3d87927684cfc1d
duexcoast <conor.ux@gmail.com>
2023-10-06 15:30:06 -0400

    Initialize app with structured logging

--- main.go
+++ main.go
@@ -1,20 +1,91 @@
 package main
 
-import "time"
+import (
+	"io"
+	"log"
+	"os"
+	"runtime/debug"
+	"strconv"
+	"time"
 
-type config struct {
-	name     string
-	email    string
-	schedule weeklySchedule
-}
-
-type weeklySchedule []shift
-
-type shift struct {
-	startTime time.Time
-	endTime   time.Time
-}
+	"github.com/unidoc/unipdf/v3/common/license"
+	"golang.org/x/exp/slog"
+)
 
 func main() {
+	appConfig, err := newAppConfig(os.Stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	common, err := newCommon(appConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	app := newApp(appConfig, common)
+	logger := app.logger
+	logger.Debug("App initialized.")
 
+	// load metered License API key prior to using the Unidoc library
+	err := license.SetMeteredKey(os.Getenv("UNIDOC_API_KEY"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type app struct {
+	*Common
+	cfg appConfig
+}
+
+type appConfig struct {
+	debug     bool
+	output    io.Writer
+	goVersion string
+	common    *Common
+	buildDate time.Time
+}
+
+type Common struct {
+	logger *slog.Logger
+}
+
+func newAppConfig(output io.Writer) (appConfig, error) {
+	buildInfo, _ := debug.ReadBuildInfo()
+	debugEnv, err := strconv.ParseBool(os.Getenv("DEBUG"))
+	if err != nil {
+		debugEnv = false
+	}
+
+	return appConfig{
+		debug:     debugEnv,
+		output:    output,
+		goVersion: buildInfo.GoVersion,
+		buildDate: time.Now(),
+	}, nil
+}
+
+func newCommon(cfg appConfig) (*Common, error) {
+	logLevel := slog.LevelInfo
+	if cfg.debug {
+		logLevel = slog.LevelDebug
+	}
+
+	opts := &slog.HandlerOptions{
+		Level: logLevel,
+	}
+
+	logger := slog.New(slog.NewTextHandler(cfg.output, opts))
+	slog.SetDefault(logger)
+
+	return &Common{
+		logger: logger,
+	}, nil
+}
+
+func newApp(cfg appConfig, common *Common) app {
+
+	return app{
+		Common: common,
+		cfg:    cfg,
+	}
 }
