9be9239d160b36a8b811dd7c3df092cb2d2fdb49
duexcoast <conor.ux@gmail.com>
2023-10-07 04:12:09 -0400

    convert csv file into []shift

--- csv_parser.go
+++ csv_parser.go
@@ -9,30 +9,105 @@ import (
 	"time"
 )
 
-func readCSVFile(filePath string) [][]string {
-	f, err := os.Open(filePath)
+type csvParser struct {
+	*Common
+	user      user
+	inputPath string
+	records   csvSchedRecords
+}
+
+func newCSVParser(inputPath string, user user, common *Common) *csvParser {
+	csvParser := csvParser{
+		Common:    common,
+		inputPath: inputPath,
+		user:      user,
+	}
+
+	return &csvParser
+}
+
+type csvSchedRecords [][]string
+
+func (c *csvParser) readCSVFile() {
+	f, err := os.Open(c.inputPath)
 	if err != nil {
-		log.Fatalf("unable to read input file %q err=%v", filePath, err)
+		log.Fatalf("unable to read input file %q err=%v", c.inputPath, err)
 	}
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
 	records, err := csvReader.ReadAll()
 	if err != nil {
-		log.Fatalf("unable to parse file as a CSV. %q err=%v", filePath, err)
+		log.Fatalf("unable to parse file as a CSV. %q err=%v", c.inputPath, err)
 	}
-	return records
+	c.records = records
+	// fmt.Printf("%v", c.records)
 }
 
-// getWeeklyHours takes the records array returned by readCSVFile and puts the
-// hours into
-// func getWeeklyHours()
+// getWeeklyHours converts an [][]string containing the entire schedule, and
+// returns []shift for csvParser.user
+func (c *csvParser) getWeeklyHours() (weeklySchedule, error) {
+	// map key is index and value is date, taken from top row of
+	// csv records
+	dateMap := map[int]string{}
+
+	fmt.Printf("LEN: %d\n", len(c.records[0]))
+
+	for i := 0; i < len(c.records[0]); i++ {
+		dateMap[i] = c.records[0][i]
+	}
+	// for k, v := range dateMap {
+	// fmt.Printf("key: %d\tvalue: %s\n", k, v)
+	// }
+
+	// this will be populated with the index of the employees hours.
+	// their hours are contained here and employeeIndex + 1
+	var employeeIndex int
+	// set match to true if we locate the employee by name
+	match := false
+
+	// loop over records and get the index of the employee
+	for i := 0; i < len(c.records); i++ {
+		fmt.Println(c.records[i][0])
+
+		// TODO: this shouldn't be hard coded. get name from user struct
+		// and convert it to the correct "Last, First" format
+		if c.records[i][0] == "Ney,Conor" {
+			match = true
+			employeeIndex = i
+		}
+	}
+	fmt.Printf("Employee Index: %d\n", employeeIndex)
+
+	if !match {
+		return nil, fmt.Errorf("Could not find the employee in the csv file. %q", c.inputPath)
+	}
+
+	// create slice to store []shift.
+	weeklySchedule := []shift{}
+
+	for i := employeeIndex; i < employeeIndex+2; i++ {
+		for j := 1; j < len(c.records[i]); j++ {
+			if c.records[i][j] != "" {
+				startTime := c.records[i][j]
+				date := dateMap[j]
+
+				shift, err := newShift(startTime, date)
+				if err != nil {
+					fmt.Println(err)
+				}
+				weeklySchedule = append(weeklySchedule, shift)
+			}
+		}
+	}
+	return weeklySchedule, nil
+}
 
 func newShift(startTime string, date string) (shift, error) {
 	// used for time.Parse, defines how to interpret string being parsed.
 	// we now need to concatenate the input strings into this format
 	// and parse them
-	dateLayout := "1/2/06 3:04pm MST"
+	dateLayout := "1/2/2006 3:04pm MST"
 
 	start := dateString(startTime, date)
 
@@ -40,6 +115,7 @@ func newShift(startTime string, date string) (shift, error) {
 
 	parsedStartTime, err := time.Parse(dateLayout, start)
 	if err != nil {
+		fmt.Println(err)
 		return shift, err
 	}
 
@@ -77,7 +153,6 @@ func newShift(startTime string, date string) (shift, error) {
 	}
 
 	shift.endTime = parsedEndTime
-	fmt.Println(parsedEndTime)
 
 	return shift, nil
 }
--- csv_parser_test.go
+++ csv_parser_test.go
@@ -1,16 +1,37 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewShift(t *testing.T) {
 	t.Run("generate shift struct", func(t *testing.T) {
-		dateString := "10/9/23"
+		dateString := "10/14/23"
 		timeString := "8:30am"
 
-		_, err := newShift(timeString, dateString)
+		shift, err := newShift(timeString, dateString)
+		t.Logf("%v\t%v", shift.startTime, shift.endTime)
 		if err != nil {
 			t.Fatalf("newShift could not complete, err=%v", err)
 		}
+	})
+
+}
+
+func TestGetWeeklyHours(t *testing.T) {
+	t.Run("parse csv file and get weekly schedule", func(t *testing.T) {
+		cfg, _ := newAppConfig(os.Stdout)
+		common, _ := newCommon(cfg)
+		user := user{
+			name:  "Conor Ney",
+			email: "conor.ux@gmail.com",
+		}
+		parser := newCSVParser("testdata/schedule.csv", user, common)
+		parser.readCSVFile()
+
+		parser.getWeeklyHours()
+
 	})
 
 }
