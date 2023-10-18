6537cb1dae83d12a488bde555070a54e9f6ae6ca
duexcoast <conor.ux@gmail.com>
2023-10-18 17:16:03 -0400

    removed obsolete row repair from csvParser

--- csv_parser.go
+++ csv_parser.go
@@ -6,16 +6,12 @@ import (
 	"log"
 	"os"
 	"path"
-	"regexp"
 	"strings"
 	"time"
 )
 
 const csvDateRegex = `(\d\d?/\d\d?/\d\d\d\d)`
 const repairCSVRegex = `(\d\d?:\d\d[pam]{2})|(oncall)|(REQUESTOFF)|(SHIFT|LEAD)`
-
-var reCSVDate = regexp.MustCompile(csvDateRegex)
-var reRepairCSV = regexp.MustCompile(repairCSVRegex)
 
 type csvParser struct {
 	*Common
@@ -55,110 +51,6 @@ func (c *csvParser) readCSVFile() csvSchedRecords {
 	return records
 }
 
-func (c *csvParser) repairCSV(records csvSchedRecords) csvSchedRecords {
-	if len(records[0]) == 8 {
-		c.logger.Debug("[repairCSV] Correct amount of rows.")
-		return nil
-	}
-	newRecords := make([][]string, 0, 42)
-
-	var friSatColumnBroken bool
-
-	// Use a regular expression to determine whether any of the first row dates
-	// contain multiple dates merged together
-	for i, v := range records[0] {
-		matches := reCSVDate.FindAllString(v, -1)
-		// if the column hasn't been split correctly, that will be indicated by
-		// more than 1 match.
-		if len(matches) > 1 {
-			if i == 5 {
-				friSatColumnBroken = true
-				dates := []string{matches[0], matches[1]}
-				// append fixed dates row
-				newRow := c.fixRow(records, dates, i, 0)
-				newRecords = append(newRecords, newRow)
-				// append Fixed Weekday row
-				days := []string{"Friday", "Saturday"}
-				newRow = c.fixRow(records, days, i, 1)
-				newRecords = append(newRecords, newRow)
-			} else {
-				c.logger.Debug("[repairCSV] The records are broken on unfixable columns: %v", matches)
-				return nil
-			}
-		}
-	}
-	// Don't return any data if we have an irrepairable row
-	if !friSatColumnBroken {
-		return nil
-	}
-	for i := 2; i < len(records); i++ {
-		data, err := splitBrokenColumn(records[i][5], 5, i)
-		if err != nil {
-			fmt.Println("[repairCSV] Could not split column")
-		}
-		fixedRow := c.fixRow(records, data, 5, i)
-		newRecords = append(newRecords, fixedRow)
-	}
-	return newRecords
-}
-
-func (c csvParser) fixRow(records csvSchedRecords, data []string, index, row int) []string {
-	// allocate the new row that we will be appending to
-	newRow := make([]string, 0, 8)
-	prevRow := records[row]
-
-	newRow = append(newRow, prevRow[0:index]...)
-	newRow = append(newRow, data...)
-	newRow = append(newRow, prevRow[index+1:]...)
-	return newRow
-}
-
-func splitBrokenColumn(cell string, col, row int) ([]string, error) {
-	// as of right now, I can only figure out how to fix a combined column
-	// that straddles Friday and Saturday, thankfully, that's the only issue
-	// I've experienced so far.
-	// Check to confirm that the broken column is friday/saturday
-	if col == 5 {
-		// if row == 1 {
-		// 	// date
-		// 	data := []string{"Friday", "Saturday"}
-		// 	return data, nil
-		// }
-		matches := reRepairCSV.FindAllString(cell, -1)
-		switch len(matches) {
-		case 0:
-			data := []string{"", ""}
-			return data, nil
-		case 1:
-			if strings.Contains(matches[0], "am") {
-				data := []string{"", matches[0]}
-				return data, nil
-			} else if strings.Contains(matches[0], "pm") {
-				data := []string{matches[0], ""}
-				return data, nil
-			} else if strings.Contains(matches[0], "oncall") {
-				// I DONT KNOW WHAT TO DO IN THIS SITUATION!
-				// There's a couple possibilities:
-				//   - oncall friday
-				//   - oncall saturday morning (we can tell it's a morning row) using
-				//     modulo math (even or odd)
-				//   - oncall saturday evening
-				//   - so basically, I'm unable to determine between oncall
-				//     friday pm and oncall saturday pm: so just mark it as both
-				//     I guess?
-				return []string{matches[0], matches[0]}, nil
-			} else {
-				return []string{"", ""}, nil
-			}
-		default:
-			return []string{matches[0], matches[1]}, nil
-		}
-	}
-
-	// shouldn't be calling this function if index isn't 5
-	return nil, fmt.Errorf("Can't fix broken column. At splitBrokenColumn")
-}
-
 // getWeeklyHours converts the parsed csv data provided by the records arg into
 // []shift, for the employee specified in csvParser.user.
 func (c *csvParser) getWeeklyHours(records csvSchedRecords) (weeklySchedule, error) {
@@ -191,7 +83,7 @@ func (c *csvParser) getWeeklyHours(records csvSchedRecords) (weeklySchedule, err
 	}
 
 	if !match {
-		return nil, fmt.Errorf("Could not find the employee in the csv file. %q", c.inputPath)
+		return nil, fmt.Errorf("Could not find the employee in the csv file: %q. Looking for: %s", c.inputPath, schedName)
 	}
 
 	// create slice to store []shift.
